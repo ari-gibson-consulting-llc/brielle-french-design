d13c6e73816df1a22e5fb62072f5a5897e98281a
Ari Gibson <hi@arimgibson.com>
2023-05-31 16:07:59 -0600

    crude FE form submission logic

--- src/lib/clientContactForm.ts
+++ src/lib/clientContactForm.ts
@@ -4,6 +4,13 @@ type ContactForm = {
   message?: string;
 };
 
+type ContactFormSubmission = {
+  name: string;
+  email: string;
+  message: string;
+  turnstileResponse: string;
+}
+
 export function validateContactFormInput(form: ContactForm): string[] {
   const requiredFields = ["name", "email", "message"] as const;
   const missingFields = requiredFields.filter((field) => !form[field]);
@@ -11,4 +18,18 @@ export function validateContactFormInput(form: ContactForm): string[] {
   return missingFields.length > 0
     ? [`Missing required fields: ${missingFields.join(", ")}`]
     : [];
+}
+
+export async function submitForm(form: ContactFormSubmission) {
+  const response = await fetch("/contactFormSubmission", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(form),
+  });
+
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
 }

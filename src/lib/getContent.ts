802fcbda5d50beea07f3c83332ed3f97c944ee43
Ari Gibson <hi@arimgibson.com>
2023-06-24 01:43:00 -0600

    add layout for static images, begin other layouts

--- src/lib/getContent.ts
+++ src/lib/getContent.ts
@@ -39,8 +39,8 @@ interface ContentfulServicesSection {
   };
 }
 
-interface ContentfulPortfolioEntryStaticImages {
-  contentTypeId: "portfolioEntry";
+export interface ContentfulPortfolioEntryStaticImages {
+  contentTypeId: "portfolioEntryStaticImages";
   fields: {
     title: contentful.EntryFieldTypes.Symbol;
     slug: contentful.EntryFieldTypes.Symbol;
@@ -49,8 +49,8 @@ interface ContentfulPortfolioEntryStaticImages {
   };
 }
 
-interface ContentfulPortfolioEntryCarousels {
-  contentTypeId: "portfolioEntry";
+export interface ContentfulPortfolioEntryCarousels {
+  contentTypeId: "portfolioEntryCarousels";
   fields: {
     title: contentful.EntryFieldTypes.Symbol;
     slug: contentful.EntryFieldTypes.Symbol;
@@ -61,8 +61,8 @@ interface ContentfulPortfolioEntryCarousels {
   };
 }
 
-interface ContentfulPortfolioEntryStaticImagesCarousels {
-  contentTypeId: "portfolioEntry";
+export interface ContentfulPortfolioEntryStaticImagesCarousels {
+  contentTypeId: "portfolioEntryStaticImagesCarousels";
   fields: {
     title: contentful.EntryFieldTypes.Symbol;
     slug: contentful.EntryFieldTypes.Symbol;
@@ -120,3 +120,41 @@ export const servicesPage = {
     };
   }),
 };
+
+export const portfolioEntries = await (async () => {
+  const entryCollections = await Promise.all([
+    contentfulClient.getEntries<ContentfulPortfolioEntryStaticImages>({
+      content_type: "portfolioEntryStaticImages",
+    }),
+    contentfulClient.getEntries<ContentfulPortfolioEntryCarousels>({
+      content_type: "portfolioEntryCarousels",
+    }),
+    contentfulClient.getEntries<ContentfulPortfolioEntryStaticImagesCarousels>({
+      content_type: "portfolioEntryStaticImagesCarousels",
+    }),
+  ]);
+
+  const entryFields = entryCollections.map((collection) =>
+    collection.items.map((entry) => ({
+      params: {
+        slug: entry.fields.slug,
+      },
+      props: {
+        ...omit("slug", entry.fields),
+        type: entry.sys.contentType.sys.id,
+        title: entry.fields.title,
+        description: snarkdown(entry.fields.description).replace(
+          /<br \/>/g,
+          "<br /><br />"
+        ),
+      },
+    }))
+  ).flat();
+
+  return entryFields;
+})();
+
+function omit<T extends object>(key: keyof T, obj: T) {
+  const { [key]: omitted, ...rest } = obj;
+  return rest;
+}

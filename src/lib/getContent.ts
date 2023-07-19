d1c8dae9f633f7291eb1a8d4fa8079aa193ed71e
Ari Gibson <hi@arimgibson.com>
2023-07-18 18:50:56 -0600

    Portfolio - Fetch Content (#10)
    * add new content types
    
    * bump deps, install typescript
    
    * add layout for static images, begin other layouts
    
    * format
    
    * clean some type errors
    
    * transferring to desktop
    
    * add field for portfolioPageDisplayImage
    
    * add new portfolio page
    
    * add fourth carousel
    
    * simplify branch to minimal changes from main
    
    * add fetching PDF portfolio entries

--- src/lib/getContent.ts
+++ src/lib/getContent.ts
@@ -39,15 +39,117 @@ interface ContentfulServicesSection {
   };
 }
 
-interface ContentfulPortfolioEntry {
-  contentTypeId: "portfolioEntry";
+interface ContentfulPortfolioPage {
+  contentTypeId: "portfolioPage";
+  fields: {
+    portfolioEntries: contentful.EntryFieldTypes.Array<
+      contentful.EntryFieldTypes.EntryLink<
+        | ContentfulPortfolioEntryStaticImages
+        | ContentfulPortfolioEntryCarousels
+        | ContentfulPortfolioEntryStaticImagesCarousels
+      >
+    >;
+  };
+}
+
+interface ContentfulPortfolioEntryStaticImages {
+  contentTypeId: "portfolioEntryStaticImages";
   fields: {
     title: contentful.EntryFieldTypes.Symbol;
-    description: contentful.EntryFieldTypes.Text;
     slug: contentful.EntryFieldTypes.Symbol;
+    description: contentful.EntryFieldTypes.Text;
+    portfolioPageDisplayImage: contentful.EntryFieldTypes.AssetLink;
     images: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.AssetLink>;
   };
 }
+
+interface ContentfulPortfolioEntryPDF {
+  contentTypeId: "portfolioEntryPdf";
+  fields: {
+    title: contentful.EntryFieldTypes.Symbol;
+    slug: contentful.EntryFieldTypes.Symbol;
+    description: contentful.EntryFieldTypes.Text;
+    portfolioPageDisplayImage: contentful.EntryFieldTypes.AssetLink;
+    pdf: contentful.EntryFieldTypes.AssetLink;
+  };
+}
+
+interface ContentfulPortfolioEntryCarousels {
+  contentTypeId: "portfolioEntryCarousels";
+  fields: {
+    title: contentful.EntryFieldTypes.Symbol;
+    slug: contentful.EntryFieldTypes.Symbol;
+    description: contentful.EntryFieldTypes.Text;
+    portfolioPageDisplayImage: contentful.EntryFieldTypes.AssetLink;
+    firstCarousel: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.AssetLink>;
+    secondCarousel?: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.AssetLink>;
+    thirdCarousel?: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.AssetLink>;
+    fourthCarousel?: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.AssetLink>;
+  };
+}
+
+interface ContentfulPortfolioEntryStaticImagesCarousels {
+  contentTypeId: "portfolioEntryStaticImagesCarousels";
+  fields: {
+    title: contentful.EntryFieldTypes.Symbol;
+    slug: contentful.EntryFieldTypes.Symbol;
+    description: contentful.EntryFieldTypes.Text;
+    portfolioPageDisplayImage: contentful.EntryFieldTypes.AssetLink;
+    images: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.AssetLink>;
+    firstCarousel: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.AssetLink>;
+    secondCarousel?: contentful.EntryFieldTypes.Array<contentful.EntryFieldTypes.AssetLink>;
+  };
+}
+
+type ContentfulAssetArr = (
+  | contentful.UnresolvedLink<"Asset">
+  | contentful.Asset<undefined, string>
+)[];
+
+interface BasePortfolioEntry {
+  params: {
+    slug: string;
+  };
+  props: {
+    slug: string;
+    title: string;
+    description: string;
+    portfolioPageDisplayImage: contentful.Asset<undefined, string>;
+  };
+}
+
+type PortfolioEntryStaticImages = BasePortfolioEntry & {
+  props: {
+    type: "portfolioEntryStaticImages";
+    images: ContentfulAssetArr;
+  };
+};
+
+type PortfolioEntryPDF = BasePortfolioEntry & {
+  props: {
+    type: "portfolioEntryPdf";
+    pdf: contentful.Asset<undefined, string>;
+  };
+};
+
+type PortfolioEntryCarousels = BasePortfolioEntry & {
+  props: {
+    type: "portfolioEntryCarousels";
+    firstCarousel: ContentfulAssetArr;
+    secondCarousel?: ContentfulAssetArr;
+    thirdCarousel?: ContentfulAssetArr;
+    fourthCarousel?: ContentfulAssetArr;
+  };
+};
+
+type PortfolioEntryStaticImagesCarousels = BasePortfolioEntry & {
+  props: {
+    type: "portfolioEntryStaticImagesCarousels";
+    images: ContentfulAssetArr;
+    firstCarousel: ContentfulAssetArr;
+    secondCarousel?: ContentfulAssetArr;
+  };
+};
 
 const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
@@ -96,3 +198,47 @@ export const servicesPage = {
     };
   }),
 };
+
+export const portfolioPage = (
+  await contentfulClient.getEntry<ContentfulPortfolioPage>(
+    "7gxjMKCsYhtBf71Ab9bwx9"
+  )
+).fields;
+
+export const portfolioEntries = await (async () => {
+  const entryCollections = await Promise.all([
+    contentfulClient.getEntries<ContentfulPortfolioEntryStaticImages>({
+      content_type: "portfolioEntryStaticImages",
+    }),
+    contentfulClient.getEntries<ContentfulPortfolioEntryPDF>({
+      content_type: "portfolioEntryPdf",
+    }),
+    contentfulClient.getEntries<ContentfulPortfolioEntryCarousels>({
+      content_type: "portfolioEntryCarousels",
+    }),
+    contentfulClient.getEntries<ContentfulPortfolioEntryStaticImagesCarousels>({
+      content_type: "portfolioEntryStaticImagesCarousels",
+    }),
+  ]);
+
+  const entryFields = entryCollections
+    .map((collection) => {
+      const type = collection.items[0].sys.contentType.sys.id;
+      return collection.items.map((entry) => ({
+        params: {
+          slug: entry.fields.slug,
+        },
+        props: {
+          ...entry.fields,
+          type,
+          description: snarkdown(entry.fields.description).replace(
+            /<br \/>/g,
+            "<br /><br />"
+          ),
+        },
+      }));
+    })
+    .flat();
+
+  return entryFields;
+})();
